1d37c55796c5c66071be7a55c76549a6b590f75e
NotItaly <thcfr1@gmail.com>
2017-05-06 10:55:39 -0400

    better brick bounce offs

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -20,11 +20,31 @@ bool Bricks::doBallCollsion(Ball & b)
 	RectF brect = b.GetRect();
 	if (!destroyed && rect.isOverLapping(brect))
 	{
-		if (brect.right > rect.left && brect.left < rect.left) //checks if hit the left side of a brick
+		if (brect.right > rect.left && brect.left < rect.left && brect.top < rect.bottom)// bottom left corner
+		{
+		//	b.ReboundX();
+			b.ReboundY();
+		}
+		else if (brect.right > rect.left && brect.left < rect.left && brect.bottom < rect.top) // top left corner
+		{
+		//	b.ReboundX();
+			b.ReboundY();
+		}
+		else if (brect.left<rect.right &&brect.right>rect.right && brect.top < rect.bottom) // bottom right
+		{
+		//	b.ReboundX();
+			b.ReboundY();
+		}
+		else if (brect.left<rect.right &&brect.right>rect.right && brect.bottom < rect.top) // top right
+		{
+		//	b.ReboundX();
+			b.ReboundY();
+		}
+		else if (brect.right > rect.left && brect.left < rect.left) //checks if hit the left side of a brick
 		{
 			b.ReboundX();
 		}
-		else if (brect.left<rect.right &&brect.right>rect.right)
+		else if (brect.left<rect.right &&brect.right>rect.right) // check if hit the right side of brick
 		{
 			b.ReboundX();
 		}
--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -26,7 +26,7 @@ Game::Game(MainWindow& wnd)
 	wnd(wnd),
 	gfx(wnd),
 	brick(RectF(271.0f, 300.0f, 240.0f, 270.0f), Colors::Cyan),
-	ball(Vec2(70, 70), Vec2(500, 500)),
+	ball(Vec2(400.0f, 493.0f), Vec2(300, -300)),
 	walls(0.0f, float(gfx.ScreenWidth), 0.0f, float(gfx.ScreenHeight)),
 	pad(Vec2(400.0f,500.0f)),
 	Sbrick(L"Sounds//arkbrick.wav"),
@@ -44,6 +44,7 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+	
 	const float dt = ft.Mark();
 	ball.Update(dt);
 	if (ball.doWallCollision(walls)) {
@@ -67,5 +68,4 @@ void Game::ComposeFrame()
 	ball.Draw(gfx);
 	brick.Draw(gfx);
 	pad.Draw(gfx);
-
 }
